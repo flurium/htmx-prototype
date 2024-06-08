762eb239e8643d579ebf891b5b364d896252d209
dimitri-fliud <valvech322@gmail.com>
2024-06-08 21:22:23 +0300

    commit

--- prototype-htmx/Pages/Index.cshtml.cs
+++ prototype-htmx/Pages/Index.cshtml.cs
@@ -1,20 +1,37 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Htmx;
+using htmx_prototype.Data;
+using Microsoft.AspNetCore.Antiforgery;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-
 namespace prototype_htmx.Pages
 {
+    public class ProductFormModel
+    {
+        public string productName { get; set; }
+        public string productAmount { get; set; }
+        public string productDescription { get; set; }
+        public string productImage { get; set; }
+    }
+
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
-
-        public IndexModel(ILogger<IndexModel> logger)
+        protected HtmxDbContext db;
+        public IndexModel(ILogger<IndexModel> logger, HtmxDbContext context)
         {
             _logger = logger;
+            db = context;
         }
 
-        public void OnGet()
+        public IActionResult OnGet()
+        {
+            return Request.IsHtmx() ? Content("<span>changed</span>", "text/html"):Page();
+        }
+
+        public IActionResult OnPostAdd([FromForm]ProductFormModel product)
         {
 
+            return Request.IsHtmx() ? Content("<span>changed</span>", "text/html") : Page();
         }
     }
 }
