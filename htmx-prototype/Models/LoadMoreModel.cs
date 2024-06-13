ecbcb84faddcad313d1c5469e3866e31e46ec083
mariia-shcherbak <masha.shcherbak.2016@gmail.com>
2024-06-14 01:45:23 +0300

    mvc

---
+++ htmx-prototype/Models/LoadMoreModel.cs
@@ -0,0 +1,8 @@
+﻿namespace htmx_prototype.Models
+{
+    public class LoadMoreModel
+    {
+        public List<Product> Products { get; set; } = new List<Product>();
+        public bool HasMore { get; set; }
+    }
+}
---
+++ htmx-prototype/Models/Product.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace htmx_prototype.Models
+{
+    public class Product
+    {
+        [Key]
+        public string Id { get; set; } = Guid.NewGuid().ToString();
+
+        [Required]
+        public string Name { get; set; }
+
+        public double Price { get; set; } = 0;
+        public string Description { get; set; } = string.Empty;
+        public string? PreviewImage { get; set; }
+    }
+}
